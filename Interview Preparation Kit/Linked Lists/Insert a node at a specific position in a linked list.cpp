be263486c853c53ad2b8cfba44018b6a13482c96
Randy Suarez Rodes <rsrjohnson@gmail.com>
2020-04-16 21:25:51 -0500

    Insert a node at a specific position in a linked list
    Added solution for Insert a node at a specific position in a linked list.

---
+++ Interview Preparation Kit/Linked Lists/Insert a node at a specific position in a linked list.cpp
@@ -0,0 +1,106 @@
+//https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+
+    SinglyLinkedListNode* n=new SinglyLinkedListNode(data);
+
+
+    if(position==0)
+    {
+        n->next=head;
+        return n;
+
+    }
+
+    int k=0;
+
+    SinglyLinkedListNode* current=head;
+
+    while(k<position-1)
+    {
+        current=current->next;
+
+        k+=1;
+    }
+    
+    SinglyLinkedListNode* temp=current->next;
+    n->next=temp;
+
+    current->next=n;
+
+
+
+    return head;
+
+
+}
