aa2eab659395baf4093dfdeef787285d56c54773
Randy Suarez Rodes <rsrjohnson@gmail.com>
2020-04-24 13:46:55 -0500

    Revert "deleted from local repository to be rename and updated."
    This reverts commit 8b3a74f12678134e8a6e03558ff0df2ce86637fc.

---
+++ 10 Days of Statistics/Day0WeightedMean.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<iomanip>
+using namespace std;
+
+
+int main() {
+    double s=0,k,t;
+    vector<double> vn;
+
+    int n;
+
+    cin>>n;
+
+    for (int i=0; i<n; i++) {
+    cin>>k;
+    vn.push_back(k);
+    }
+
+    for (int i=0; i<n; i++) {
+    cin>>k;
+    s+=k;
+    t+=k*vn[i];
+    }
+
+
+    cout<<setprecision(1)<<fixed<<t/s;
+
+    return 0;
+}
